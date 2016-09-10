f380c43c996010920fbcbe10790cdd77c6498f7a
tdeboissiere <thibault.deboissiere@seeingmachines.com>
2016-09-10 21:21:44 +1000

    Add cifar

--- DenseNet/densenet.py
+++ DenseNet/densenet.py
@@ -1,32 +1,30 @@
-from keras.models import Sequential, Model
-from keras.layers.core import Flatten, Dense, Dropout, Activation
+from keras.models import Model
+from keras.layers.core import Flatten, Reshape, Dense, Dropout, Activation
 from keras.layers.convolutional import Convolution2D
-from keras.layers.convolutional import ZeroPadding2D
-from keras.layers.convolutional import UpSampling2D
-from keras.layers.convolutional import Deconvolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.pooling import AveragePooling2D
 from keras.layers import Input, merge
-from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.normalization import BatchNormalization
 
 
-def conv_factory(x, num_filter, dropout_rate=None):
+def conv_factory(x, nb_filter, dropout_rate=None):
+    """Apply BatchNorm, Relu 3x3Conv2D, optional dropout"""
 
     x = BatchNormalization(mode=2, axis=1)(x)
     x = Activation('relu')(x)
-    x = Convolution2D(num_filter, 3, 3, border_mode="same")(x)
+    x = Convolution2D(nb_filter, 3, 3, border_mode="same")(x)
     if dropout_rate:
         x = Dropout(dropout_rate)(x)
 
     return x
 
 
-def transition(x, num_filter, dropout_rate=None):
+def transition(x, nb_filter, dropout_rate=None):
+    """Apply BatchNorm, Relu 1x1Conv2D, optional dropout and Maxpooling2D"""
 
     x = BatchNormalization(mode=2, axis=1)(x)
     x = Activation('relu')(x)
-    x = Convolution2D(num_filter, 1, 1, border_mode="same")(x)
+    x = Convolution2D(nb_filter, 1, 1, border_mode="same")(x)
     if dropout_rate:
         x = Dropout(dropout_rate)(x)
     x = MaxPooling2D((2, 2), strides=(2, 2))(x)
@@ -34,34 +32,46 @@ def transition(x, num_filter, dropout_rate=None):
     return x
 
 
-def denseblock(x, num_conv, num_filter, dropout_rate=None):
+def denseblock(x, nb_layers, nb_filter, growth_rate, dropout_rate=None):
+    """Build a denseblock where the output of each conv_factory is fed to subsequent ones"""
 
     list_feat = []
 
-    for i in range(num_conv):
-        x = conv_factory(x, num_filter, dropout_rate)
+    for i in range(nb_layers):
+        x = conv_factory(x, nb_filter, dropout_rate)
         list_feat.append(x)
-        x = merge(list_feat, mode='concat', concat_axis=1)
+        if len(list_feat) > 1:
+            x = merge(list_feat, mode='concat', concat_axis=1)
+        nb_filter += growth_rate
 
-    x = transition(x, num_filter, dropout_rate=dropout_rate)
-
-    return x
+    return x, nb_filter
 
 
-def DenseNet(nb_classes, img_dim, num_dense_block, num_conv, num_filter, dropout_rate=None):
+def DenseNet(nb_classes, img_dim, depth, nb_dense_block, growth_rate, nb_filter, dropout_rate=None):
+    """ Build the DenseNet model"""
 
     model_input = Input(shape=img_dim)
 
+    assert (depth - 4) % 3 == 0, "Depth must be 3 N + 4"
+
+    # layers in each dense block
+    nb_layers = (depth - 4) / 3
+
     # Initial convolution
-    x = Convolution2D(16, 3, 3, border_mode="same")(model_input)
+    x = Convolution2D(nb_filter, 3, 3, border_mode="same")(model_input)
 
     # Add dense blocks
-    for i in range(num_dense_block):
-        x = denseblock(x, num_conv, num_filter, dropout_rate=dropout_rate)
+    for i in range(nb_dense_block - 1):
+        x, nb_filter = denseblock(x, nb_layers, nb_filter, growth_rate, dropout_rate=dropout_rate)
+        # add transition
+        x = transition(x, nb_filter, dropout_rate=dropout_rate)
+
+    # The last denseblock does not have a transition
+    x, nb_filter = denseblock(x, nb_layers, nb_filter, growth_rate, dropout_rate=dropout_rate)
 
     x = BatchNormalization(mode=2, axis=1)(x)
     x = Activation('relu')(x)
-    x = AveragePooling2D((8,8))
+    x = AveragePooling2D((8,8))(x)
     x = Flatten()(x)
     x = Dense(nb_classes, activation='softmax')(x)
 
---
+++ DenseNet/run_cifar.py
@@ -0,0 +1,54 @@
+
+
+from __future__ import print_function
+from keras.datasets import cifar10
+from keras.optimizers import Adam
+from keras.utils import np_utils
+import densenet
+
+batch_size = 32
+nb_classes = 10
+nb_epoch = 200
+data_augmentation = True
+
+# input image dimensions
+img_rows, img_cols = 32, 32
+# the CIFAR10 images are RGB
+img_channels = 3
+
+# the data, shuffled and split between train and test sets
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+img_dim = (img_channels, img_rows, img_cols)
+depth = 19
+nb_dense_block = 3
+growth_rate = 12
+nb_filter = 16
+
+model = densenet.DenseNet(nb_classes,
+                          img_dim,
+                          depth,
+                          nb_dense_block,
+                          growth_rate,
+                          nb_filter,
+                          dropout_rate=None)
+
+# Model output
+model.summary()
+
+# Build optimizer
+opt = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+
+model.compile(loss='categorical_crossentropy', optimizer=opt)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255.
+X_test /= 255.
+
+print("Training")
+model.fit(X_train, Y_train, nb_epoch=nb_epoch, validation_data=(X_test, Y_test), verbose=1)
