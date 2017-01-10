deb413193d26bc35bd7fe10159714dd6271ad96f
tdeboissiere <thibault.deboissiere@seeingmachines.com>
2017-01-10 11:17:13 +1100

    Update figures

--- InfoGAN/src/model/eval.py
+++ InfoGAN/src/model/eval.py
@@ -28,8 +28,8 @@ def eval(**kwargs):
     general_utils.setup_logging(model_name)
 
     # Load and rescale data
-    if dset == "celebA":
-        X_real_train = data_utils.load_celebA(img_dim, image_dim_ordering)
+    if dset == "RGZ":
+        X_real_train = data_utils.load_RGZ(img_dim, image_dim_ordering)
     if dset == "mnist":
         X_real_train, _, _, _ = data_utils.load_mnist(image_dim_ordering)
     img_dim = X_real_train.shape[-3:]
@@ -53,6 +53,7 @@ def eval(**kwargs):
     for i in range(cat_dim[0]):
         X_noise = data_utils.sample_noise(noise_scale, batch_size, noise_dim)
         X_cont = data_utils.sample_noise(noise_scale, batch_size, cont_dim)
+        X_cont = np.repeat(X_cont[:1, :], batch_size, axis=0)  # fix continuous noise
         X_cat = np.zeros((batch_size, cat_dim[0]), dtype='float32')
         X_cat[:, i] = 1  # always the same categorical value
 
@@ -66,11 +67,19 @@ def eval(**kwargs):
         X_plot.append(np.concatenate(X_gen, axis=1))
     X_plot = np.concatenate(X_plot, axis=0)
 
+    plt.figure(figsize=(8,10))
     if X_plot.shape[-1] == 1:
         plt.imshow(X_plot[:, :, 0], cmap="gray")
     else:
         plt.imshow(X_plot)
-    plt.savefig("../../figures/varying_categorical.png")
+    plt.xticks([])
+    plt.yticks([])
+    plt.ylabel("Varying categorical factor", fontsize=28, labelpad=60)
+
+    plt.annotate('', xy=(-0.05, 0), xycoords='axes fraction', xytext=(-0.05, 1),
+                 arrowprops=dict(arrowstyle="-|>", color='k', linewidth=4))
+    plt.tight_layout()
+    plt.savefig("../../figures/varying_categorical.png", dpi=300)
     plt.clf()
     plt.close()
 
@@ -79,12 +88,12 @@ def eval(**kwargs):
     # First get the extent of the noise sampling
     x = np.ravel(data_utils.sample_noise(noise_scale, batch_size * 20000, cont_dim))
     # Define interpolation points
-    x = np.linspace(2 * x.min(), 2 * x.max(), num=batch_size)
+    x = np.linspace(x.min(), x.max(), num=batch_size)
     for i in range(batch_size):
         X_noise = data_utils.sample_noise(noise_scale, batch_size, noise_dim)
         X_cont = np.concatenate([np.array([x[i], x[j]]).reshape(1, -1) for j in range(batch_size)], axis=0)
         X_cat = np.zeros((batch_size, cat_dim[0]), dtype='float32')
-        X_cat[:, 5] = 1  # always the same categorical value
+        X_cat[:, 1] = 1  # always the same categorical value
 
         X_gen = generator_model.predict([X_cat, X_cont, X_noise])
         X_gen = data_utils.inverse_normalization(X_gen)
@@ -94,10 +103,20 @@ def eval(**kwargs):
         X_plot.append(np.concatenate(X_gen, axis=1))
     X_plot = np.concatenate(X_plot, axis=0)
 
+    plt.figure(figsize=(10,10))
     if X_plot.shape[-1] == 1:
         plt.imshow(X_plot[:, :, 0], cmap="gray")
     else:
         plt.imshow(X_plot)
+    plt.xticks([])
+    plt.yticks([])
+    plt.ylabel("Varying continuous factor 1", fontsize=28, labelpad=60)
+    plt.annotate('', xy=(-0.05, 0), xycoords='axes fraction', xytext=(-0.05, 1),
+                 arrowprops=dict(arrowstyle="-|>", color='k', linewidth=4))
+    plt.xlabel("Varying continuous factor 2", fontsize=28, labelpad=60)
+    plt.annotate('', xy=(1, -0.05), xycoords='axes fraction', xytext=(0, -0.05),
+                 arrowprops=dict(arrowstyle="-|>", color='k', linewidth=4))
+    plt.tight_layout()
     plt.savefig("../../figures/varying_continuous.png")
     plt.clf()
     plt.close()
