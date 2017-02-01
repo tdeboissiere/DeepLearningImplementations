e5ad5bd6b736a9473bf490918605862353c7eaa7
tdeboissiere <thibault.deboissiere@seeingmachines.com>
2017-02-01 19:30:47 +1100

    add mog

---
+++ WassersteinGAN/figures/write_gif_script.py
@@ -0,0 +1,10 @@
+import glob
+from natsort import natsorted
+
+list_toy = natsorted(glob.glob("*toy_dataset_iter*"))
+list_toy_str = " ".join(list_toy)
+
+with open("make_gif.sh", "w") as f:
+
+    cmd = "convert -delay 30 -resize 600x600 -loop 0 %s MoG_dataset.gif" % list_toy_str
+    f.write(cmd)
