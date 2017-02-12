a692317b9805efecbd4174efd15a38e40beca5e3
tdeboissiere <thibault.deboissiere@seeingmachines.com>
2017-02-12 14:37:35 +1100

    pdate pix2pix

--- pix2pix/src/data/README.md
+++ pix2pix/src/data/README.md
@@ -12,24 +12,27 @@ You should have the following folder structure:
     ├── facades
         ├── train 
         ├── test 
-        ├── val 
-        ├── list_landmarks_celeba.txt
+        ├── val
 
 
-# Step 2. Build HDF5 CelebA dataset
+# Step 2. Build HDF5 facades dataset
 
 `python make_dataset.py`
 
 positional arguments:
-  jpeg_dir             path to celeba jpeg images
+
+    jpeg_dir             path to jpeg images
+    nb_channels          number of image channels
 
 optional arguments:
-  -h, --help           show this help message and exit
-  --img_size IMG_SIZE  Desired Width == Height
-  --do_plot DO_PLOT    Plot the images to make sure the data processing went
-                       OK
+
+    -h, --help           show this help message and exit
+    --img_size IMG_SIZE  Desired Width == Height
+    --do_plot            Plot the images to make sure the data processing went
+                         OK
+
 
 
 **Example:**
 
-`python make_dataset.py --img_size 256 --do_plot True`
+`python make_dataset.py /home/user/GitHub/pix2pix/datasets/facades 3 --img_size 256 --do_plot True`
--- pix2pix/src/data/make_dataset.py
+++ pix2pix/src/data/make_dataset.py
@@ -9,23 +9,30 @@ from tqdm import tqdm as tqdm
 import matplotlib.pylab as plt
 
 
-def format_image(img_path, size):
+def format_image(img_path, size, nb_channels):
     """
     Load img with opencv and reshape
     """
 
-    img_color = cv2.imread(img_path)
-    img_color = img_color[:, :, ::-1]
+    if nb_channels == 1:
+        img = cv2.imread(img_path, 0)
+        img = np.expand_dims(img, axis=-1)
+    else:
+        img = cv2.imread(img_path)
+        img = img[:, :, ::-1]  # GBR to RGB
 
-    w = img_color.shape[1]
+    w = img.shape[1]
 
     # Slice image in 2 to get both parts
-    img_full = img_color[:, :w / 2, :]
-    img_sketch = img_color[:, w / 2:, :]
+    img_full = img[:, :w / 2, :]
+    img_sketch = img[:, w / 2:, :]
 
-    if size != 256:
-        img_full = cv2.resize(img_full, (size, size), interpolation=cv2.INTER_AREA)
-        img_sketch = cv2.resize(img_sketch, (size, size), interpolation=cv2.INTER_AREA)
+    img_full = cv2.resize(img_full, (size, size), interpolation=cv2.INTER_AREA)
+    img_sketch = cv2.resize(img_sketch, (size, size), interpolation=cv2.INTER_AREA)
+
+    if nb_channels == 1:
+        img_full = np.expand_dims(img_full, -1)
+        img_sketch = np.expand_dims(img_sketch, -1)
 
     img_full = np.expand_dims(img_full, 0).transpose(0, 3, 1, 2)
     img_sketch = np.expand_dims(img_sketch, 0).transpose(0, 3, 1, 2)
@@ -33,28 +40,30 @@ def format_image(img_path, size):
     return img_full, img_sketch
 
 
-def build_HDF5(jpeg_dir, size=256):
+def build_HDF5(jpeg_dir, nb_channels, size=256):
     """
     Gather the data in a single HDF5 file.
     """
 
     # Put train data in HDF5
-    hdf5_file = os.path.join(data_dir, "facade_data.h5")
+    file_name = os.path.basename(jpeg_dir.rstrip("/"))
+    hdf5_file = os.path.join(data_dir, "%s_data.h5" % file_name)
     with h5py.File(hdf5_file, "w") as hfw:
 
         for dset_type in ["train", "test", "val"]:
 
-            list_img = Path(jpeg_dir).glob('%s/*.jpg' % dset_type)
+            list_img = list(Path(jpeg_dir).glob('%s/*.jpg' % dset_type))
+            list_img.extend(list(Path(jpeg_dir).glob('%s/*.png' % dset_type)))
             list_img = map(str, list_img)
             list_img = np.array(list_img)
 
             data_full = hfw.create_dataset("%s_data_full" % dset_type,
-                                           (0, 3, size, size),
+                                           (0, nb_channels, size, size),
                                            maxshape=(None, 3, size, size),
                                            dtype=np.uint8)
 
             data_sketch = hfw.create_dataset("%s_data_sketch" % dset_type,
-                                             (0, 3, size, size),
+                                             (0, nb_channels, size, size),
                                              maxshape=(None, 3, size, size),
                                              dtype=np.uint8)
 
@@ -66,7 +75,7 @@ def build_HDF5(jpeg_dir, size=256):
             for chunk_idx in tqdm(arr_chunks):
 
                 list_img_path = list_img[chunk_idx].tolist()
-                output = parmap.map(format_image, list_img_path, size, parallel=True)
+                output = parmap.map(format_image, list_img_path, size, nb_channels, parallel=False)
 
                 arr_img_full = np.concatenate([o[0] for o in output], axis=0)
                 arr_img_sketch = np.concatenate([o[1] for o in output], axis=0)
@@ -78,14 +87,14 @@ def build_HDF5(jpeg_dir, size=256):
                 data_full[-arr_img_full.shape[0]:] = arr_img_full.astype(np.uint8)
                 data_sketch[-arr_img_sketch.shape[0]:] = arr_img_sketch.astype(np.uint8)
 
-
-def check_HDF5():
+def check_HDF5(jpeg_dir, nb_channels):
     """
     Plot images with landmarks to check the processing
     """
 
     # Get hdf5 file
-    hdf5_file = os.path.join(data_dir, "facade_data.h5")
+    file_name = os.path.basename(jpeg_dir.rstrip("/"))
+    hdf5_file = os.path.join(data_dir, "%s_data.h5" % file_name)
 
     with h5py.File(hdf5_file, "r") as hf:
         data_full = hf["train_data_full"]
@@ -94,7 +103,11 @@ def check_HDF5():
             plt.figure()
             img = data_full[i, :, :, :].transpose(1,2,0)
             img2 = data_sketch[i, :, :, :].transpose(1,2,0)
-            plt.imshow(np.concatenate((img, img2), axis=1))
+            img = np.concatenate((img, img2), axis=1)
+            if nb_channels == 1:
+                plt.imshow(img[:, :, 0], cmap="gray")
+            else:
+                plt.imshow(img)
             plt.show()
             plt.clf()
             plt.close()
@@ -103,16 +116,17 @@ def check_HDF5():
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Build dataset')
-    parser.add_argument('jpeg_dir', type=str, help='path to celeba jpeg images')
+    parser.add_argument('jpeg_dir', type=str, help='path to jpeg images')
+    parser.add_argument('nb_channels', type=int, help='number of image channels')
     parser.add_argument('--img_size', default=256, type=int,
                         help='Desired Width == Height')
-    parser.add_argument('--do_plot', default=False, type=bool,
+    parser.add_argument('--do_plot', action="store_true",
                         help='Plot the images to make sure the data processing went OK')
     args = parser.parse_args()
 
     data_dir = "../../data/processed"
 
-    build_HDF5(args.jpeg_dir, size=args.img_size)
+    build_HDF5(args.jpeg_dir, args.nb_channels, size=args.img_size)
 
     if args.do_plot:
-        check_HDF5()
+        check_HDF5(args.jpeg_dir, args.nb_channels)
