021c0ea7cc8a585b79052c234b04fd9a56aaba55
tdeboissiere <thibault.deboissiere@seeingmachines.com>
2017-02-18 22:09:51 +1100

    Correct typo WGAN

--- WassersteinGAN/src/model/main.py
+++ WassersteinGAN/src/model/main.py
@@ -15,12 +15,13 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Train model')
     parser.add_argument('--backend', type=str, default="theano", help="theano or tensorflow")
-    parser.add_argument('--generator', type=str, default="upsampling", help="upsampling or deconv")
+    parser.add_argument('--generator', type=str, default="upsampling", help="upsampling or deconv or subpixel")
+    parser.add_argument('--discriminator', type=str, default="discriminator", help="discriminator discriminator_resnet")
     parser.add_argument('--dset', type=str, default="mnist", help="mnist or celebA or cifar10 or toy")
     parser.add_argument('--img_dim', default=64, type=int, help="Image width == height")
-    parser.add_argument('--nb_epoch', default=400, type=int, help="Number of batches per epoch")
+    parser.add_argument('--nb_epoch', default=400, type=int, help="Number of epochs")
     parser.add_argument('--batch_size', default=32, type=int, help='Batch size')
-    parser.add_argument('--n_batch_per_epoch', default=200, type=int, help="Number of training epochs")
+    parser.add_argument('--n_batch_per_epoch', default=200, type=int, help="Number of batch per epochs")
     parser.add_argument('--bn_mode', default=2, type=int, help="Batch norm mode")
     parser.add_argument('--noise_dim', default=100, type=int, help="noise sampler dimension")
     parser.add_argument('--noise_scale', default=0.5, type=float, help="noise sampler variance")
@@ -31,12 +32,13 @@ if __name__ == "__main__":
     parser.add_argument('--opt_G', type=str, default="RMSprop", help="Optimizer for the generator")
     parser.add_argument('--lr_D', type=float, default=5E-5, help="learning rate for the discriminator")
     parser.add_argument('--lr_G', type=float, default=5E-5, help="learning rate for the generator")
+    parser.add_argument('--use_mbd', action="store_true", help="use mini batch disc")
 
     args = parser.parse_args()
 
     assert args.dset in ["mnist", "celebA", "cifar10", "toy"]
-    assert args.opt_G in ["RMSprop", "SGD", "Adam"], "Unsupported optimizer"
-    assert args.opt_D in ["RMSprop", "SGD", "Adam"], "Unsupported optimizer"
+    assert args.opt_G in ["RMSprop", "SGD", "Adam", "AdamWithWeightnorm"], "Unsupported optimizer"
+    assert args.opt_D in ["RMSprop", "SGD", "Adam", "AdamWithWeightnorm"], "Unsupported optimizer"
 
     # Set the backend by modifying the env variable
     if args.backend == "theano":
@@ -59,6 +61,7 @@ if __name__ == "__main__":
 
     # Set default params
     d_params = {"generator": args.generator,
+                "discriminator": args.discriminator,
                 "dset": args.dset,
                 "img_dim": args.img_dim,
                 "nb_epoch": args.nb_epoch,
@@ -74,6 +77,7 @@ if __name__ == "__main__":
                 "lr_G": args.lr_G,
                 "opt_D": args.opt_D,
                 "opt_G": args.opt_G,
+                "use_mbd": args.use_mbd,
                 "image_dim_ordering": image_dim_ordering
                 }
 
